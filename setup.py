c1e172eb919adb99c7d6eeaa5420abd7a8189afa
btmanm <btmanm@gmail.com>
2016-06-07 15:23:44 +0800

    change to windows

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import sys
+
 from distutils.core import setup
 import py2exe
  
-setup(console=['main.py',
-               ])
+sys.argv.append('py2exe')
+
+setup(
+    windows = ['main.py'],
+)
